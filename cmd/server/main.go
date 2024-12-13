73046d40f3233a3810eb69812e4c1a2627102c1c
Alexander Renz <renz@uuxo.net>
2024-12-13 09:19:56 +0100

    Refactor scan worker initialization to log worker IDs for better tracking

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -945,10 +945,12 @@ func scanWorker(ctx context.Context, workerID int) {
 }
 
 func initializeScanWorkerPool(ctx context.Context) {
-	for i := 0; i < conf.ClamAV.NumScanWorkers; i++ {
-		go scanWorker(ctx, i)
-	}
-	log.Infof("Initialized %d scan workers", conf.ClamAV.NumScanWorkers)
+    var workerIDs []int
+    for i := 0; i < conf.ClamAV.NumScanWorkers; i++ {
+        go scanWorker(ctx, i)
+        workerIDs = append(workerIDs, i)
+    }
+    log.Infof("Initialized %d scan workers: %v", conf.ClamAV.NumScanWorkers, workerIDs)
 }
 
 func setupRouter() http.Handler {
