7460a31b1000b0a37269f98748ab40e4a3468332
Alexander Renz <renz@uuxo.net>
2024-12-15 11:45:52 +0100

    Refactor upload handler to support file validation, chunked uploads, and enhanced logging

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,18 @@
 package handlers
 
 import (
+    "io"
     "net/http"
+    "os"
+    "path/filepath"
+    "strings"
+
+    "github.com/sirupsen/logrus"
     "github.com/PlusOne/hmac-file-server/config"
     "github.com/PlusOne/hmac-file-server/workers"
 )
 
-func UploadHandler(w http.ResponseWriter, r *http.Request) {
+func UploadHandlerV2(w http.ResponseWriter, r *http.Request) {
     // Implement the upload handler logic here
     w.WriteHeader(http.StatusOK)
     w.Write([]byte("Upload successful"))
@@ -14,4 +20,90 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 func InitHandlers(uploadQueue chan workers.UploadTask, scanQueue chan workers.ScanTask, conf *config.Config) {
     // Initialize handlers with access to the queues and configuration
+}
+
+type Config struct {
+    Server struct {
+        ListenPort           string
+        StoragePath          string
+        DeduplicationEnabled bool
+        MinFreeBytes         string
+    }
+    Uploads struct {
+        ChunkedUploadsEnabled bool
+        ChunkSize             string
+        AllowedExtensions     []string
+    }
+    Redis struct {
+        RedisEnabled             bool
+        RedisDBIndex             int
+        RedisAddr                string
+        RedisPassword            string
+        RedisHealthCheckInterval string
+    }
+    File struct {
+        FileRevision int
+    }
+}
+
+// Example function where you might want to check the ListenPort value
+func handleUpload(w http.ResponseWriter, r *http.Request, conf Config) {
+    if conf.Server.ListenPort == "8080" {
+        logrus.Info("ListenPort is set to 8080.")
+        // Add your logic here
+    } else {
+        logrus.Infof("ListenPort is set to %s.", conf.Server.ListenPort)
+    }
+
+    if conf.File.FileRevision == 1 {
+        logrus.Info("FileRevision is set to 1.")
+        // Add your logic here
+    } else {
+        logrus.Infof("FileRevision is set to %d.", conf.File.FileRevision)
+    }
+
+    if !conf.Redis.RedisEnabled {
+        logrus.Info("Redis is disabled.")
+    } else {
+        logrus.Infof("Redis is enabled. Addr: %s, DBIndex: %d, HealthCheckInterval: %s",
+            conf.Redis.RedisAddr, conf.Redis.RedisDBIndex, conf.Redis.RedisHealthCheckInterval)
+    }
+
+    file, header, err := r.FormFile("file")
+    if err != nil {
+        http.Error(w, "Failed to get file from request", http.StatusBadRequest)
+        return
+    }
+    defer file.Close()
+
+    // Check if the file extension is allowed
+    ext := strings.ToLower(filepath.Ext(header.Filename))
+    allowed := false
+    for _, allowedExt := range conf.Uploads.AllowedExtensions {
+        if ext == allowedExt {
+            allowed = true
+            break
+        }
+    }
+    if !allowed {
+        http.Error(w, "File extension not allowed", http.StatusForbidden)
+        return
+    }
+
+    chunkSize, err := parseChunkSize(conf.Uploads.ChunkSize)
+    if err != nil {
+        http.Error(w, "Invalid chunk size", http.StatusInternalServerError)
+        return
+    }
+
+    filePath := filepath.Join(conf.Server.StoragePath, header.Filename)
+    outFile, err := os.Create(filePath)
+    if err != nil {
+        http.Error(w, "Failed to create file", http.StatusInternalServerError)
+        return
+    }
+    defer outFile.Close()
+
+    buf := make([]byte, chunkSize)
+    // Rest of your upload handling code
 }
