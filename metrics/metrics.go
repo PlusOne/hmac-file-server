84cae3816619db5f0281c9ec81fd2413dfe85c47
Alexander Renz <renz@uuxo.net>
2024-12-15 18:28:12 +0100

    Refactor metrics initialization to streamline metric registration and remove unused metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -4,30 +4,17 @@ import (
     "sync"
 
     "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promauto"
     "github.com/sirupsen/logrus"
 )
 
 var (
-    opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-        Name: "myapp_processed_ops_total",
-        Help: "The total number of processed events",
-    })
     once sync.Once
+
+    // Define your metrics
     uploadErrorsTotal = prometheus.NewCounter(
         prometheus.CounterOpts{
             Name: "upload_errors_total",
             Help: "Total number of upload errors",
-        },
-    )
-    // Use sync.Once to ensure metrics are registered only once
-    initOnce sync.Once
-
-    // Define your metrics
-    UploadCounter = prometheus.NewCounter(
-        prometheus.CounterOpts{
-            Name: "upload_counter",
-            Help: "Number of file uploads",
         },
     )
     // ... other metrics ...
@@ -35,18 +22,9 @@ var (
 
 func InitMetrics() {
     once.Do(func() {
-        // Register Prometheus metrics
+        // Register metrics
         prometheus.MustRegister(uploadErrorsTotal)
+        // Register other metrics
         logrus.Info("Prometheus metrics initialized.")
     })
-    initOnce.Do(func() {
-        // Register metrics
-        prometheus.MustRegister(UploadCounter)
-        // Register other metrics
-    })
-}
-
-func UpdateMetrics() {
-    // Update your metrics here
-    opsProcessed.Inc()
 }
